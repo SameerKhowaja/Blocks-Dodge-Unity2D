31aca2e52b7ba99e2d34eb9abc6147471f9352cc
Sameer Khowaja <sameerkhowaja12@gmail.com>
2020-01-30 22:49:54 +0500

    Mobile conversion and First Potrait Work

--- Blocks Dodge Unity2D/Assets/Script/modes.cs
+++ Blocks Dodge Unity2D/Assets/Script/modes.cs
@@ -8,8 +8,10 @@ public class modes : MonoBehaviour
     public score sc;
     public enemyCreateOnRandom ecr;
     public playerMove pm;
-    public Text start, highscore;
+    public Text highscore;
     public AudioSource aud;
+
+    public Canvas ButtonCanvas;
 
     private void Start()
     {
@@ -18,23 +20,26 @@ public class modes : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey("escape"))
-        {
-            Application.Quit();
-        }
-
-        if (Input.GetKey("s"))
-        {
-            sc.enabled = ecr.enabled = pm.enabled = true;
-            start.enabled = false;
-            aud.enabled = true;
-        }
-
-        if (Input.GetKey("r"))
+        if (Input.GetKey(KeyCode.R))
         {
             PlayerPrefs.DeleteKey("Highscore");
             highscore.text = PlayerPrefs.GetInt("Highscore", 0).ToString();
         }
 
     }
+
+
+    public void ExitBTN()
+    {
+        Application.Quit();
+    }
+
+    public void StartBTN()
+    {
+        ButtonCanvas.enabled = false;
+
+        sc.enabled = ecr.enabled = pm.enabled = true;
+        aud.enabled = true;
+    }
+
 }
--- Blocks Dodge Unity2D/Assets/Script/playerMove.cs
+++ Blocks Dodge Unity2D/Assets/Script/playerMove.cs
@@ -6,17 +6,39 @@ using UnityEngine.SceneManagement;
 public class playerMove : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float speed = 20f;
-    float xPos, maxWidth = 4.8f;
+    public float speed;
+    float xPos, maxWidth = 2.4f;
     Vector2 xPosNew;
+    float XPosInput;
     public enemyCreateOnRandom enemyScript;
     public GameObject go;
     public AudioSource aud;
     public float slowness = 20f;
 
+    Touch touch;
+    Vector3 touchPos;
+
+    private void Update()
+    {
+        XPosInput = Input.GetAxis("Horizontal");
+
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touchPos.x > 0)
+                XPosInput = 1;
+            else if (touchPos.x < 0)
+                XPosInput = -1;
+            else
+                XPosInput = 0;
+        }
+    }
+
     void FixedUpdate()
     {
-        xPos = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
+        xPos = XPosInput * speed * Time.fixedDeltaTime;
         xPosNew = rb.position + Vector2.right * xPos;
         xPosNew.x = Mathf.Clamp(xPosNew.x, -maxWidth, maxWidth);
         rb.MovePosition(xPosNew);
